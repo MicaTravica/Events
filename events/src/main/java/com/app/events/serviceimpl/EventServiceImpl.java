edc9fc93fe789b64cf702590d8caaf5ca7012ab0
DusanBucan <dusanbzr@gmail.com>
2019-11-03 14:01:38 +0100

    fix master

--- events/src/main/java/com/app/events/serviceimpl/EventServiceImpl.java
+++ events/src/main/java/com/app/events/serviceimpl/EventServiceImpl.java
@@ -2,6 +2,9 @@ package com.app.events.serviceimpl;
 
 import com.app.events.service.EventService;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventServiceImpl implements EventService {
 
 }
