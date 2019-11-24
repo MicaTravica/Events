ea81de038cab80046b357c9e5169ead011d4e2b9
DusanBucan <dusanbzr@gmail.com>
2019-11-24 20:11:15 +0100

    testController

--- events/src/test/java/com/app/events/controller/HallControlerUnitTest.java
+++ events/src/test/java/com/app/events/controller/HallControlerUnitTest.java
@@ -7,19 +7,26 @@ import com.app.events.dto.HallDTO;
 import com.app.events.exception.ResourceNotFoundException;
 import com.app.events.mapper.HallMapper;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.ResourceAccessException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -32,9 +39,12 @@ import java.util.HashSet;
 
 
 
-
+@Import(authConfig.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+
+// @WebMvcTest(controllers = HallController.class)
+//  ima problem nece da ucita userDetailsService za auth...
 
 // @ActiveProfiles("test")
 // @TestPropertySource(locations = "classpath:application-test.properties")
@@ -46,6 +56,9 @@ public class HallControlerUnitTest {
     public static long INVALID_HALL_ID = -1L;
     
     public static long PLACE_ID = 1L;
+
+    // @Autowired
+	// private MockMvc mockMvc;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -63,10 +76,14 @@ public class HallControlerUnitTest {
     }
 
 
-    @Ignore
     @Test
     @WithMockUser(username = "dusan", password = "cao", roles = {"REGULAR"})
     public void whenValidId_thenFoundHall() throws Exception{
+
+
+        // mockMvc.perform(get("/api/hall/1")).andExpect(status().isOk())
+		// 		.andExpect(jsonPath("$.id").value(1l)).andExpect(jsonPath("$.name").value("Alex"));
+
         ResponseEntity<HallDTO> responseEntity =
 	        restTemplate.getForEntity("/api/hall/"+ HALL_FIND_ID, HallDTO.class);
         Hall hall = HallMapper.toHall(responseEntity.getBody());
@@ -74,10 +91,16 @@ public class HallControlerUnitTest {
 		assertEquals(hall.getId(), HALL_FIND_ID);
     }
 
-    @Ignore
     @Test(expected = ResourceNotFoundException.class)
     @WithMockUser(username = "user1", password = "pwd", roles = {"REGULAR"})
     public void whenInvalidId_thenThowException() throws Exception{
+        
+        
+        // mockMvc.perform(get("/api/hall/1")).andExpect(status().isOk())
+		// 		.andExpect(jsonPath("$.id").value(1l)).andExpect(jsonPath("$.name").value("Alex"));
+
+        
+        
         ResponseEntity<HallDTO> responseEntity =
             restTemplate.getForEntity("/api/hall/" + INVALID_HALL_ID, HallDTO.class);
             
