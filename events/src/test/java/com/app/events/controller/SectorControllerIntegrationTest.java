a1a15897c4c021e99df4996116628a3ba8404195
DusanBucan <dusanbzr@gmail.com>
2019-12-08 19:21:00 +0100

    mod 5

--- events/src/test/java/com/app/events/controller/SectorControllerIntegrationTest.java
+++ events/src/test/java/com/app/events/controller/SectorControllerIntegrationTest.java
@@ -37,7 +37,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-@TestPropertySource("classpath:application-test.properties")
+@TestPropertySource("classpath:application.properties")
 public class SectorControllerIntegrationTest {
 
     public static URI uri;
