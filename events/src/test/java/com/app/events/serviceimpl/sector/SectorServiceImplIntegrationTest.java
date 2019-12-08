4d26cf814bc0aac940ea6ed76aa7a7cf61b35333
DusanBucan <dusanbzr@gmail.com>
2019-12-08 19:15:24 +0100

    mod 3

--- events/src/test/java/com/app/events/serviceimpl/sector/SectorServiceImplIntegrationTest.java
+++ events/src/test/java/com/app/events/serviceimpl/sector/SectorServiceImplIntegrationTest.java
@@ -1,8 +1,16 @@
 package com.app.events.serviceimpl.sector;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 /**
  * SectorServiceImplIntegrationTest
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
 public class SectorServiceImplIntegrationTest {
 
     
