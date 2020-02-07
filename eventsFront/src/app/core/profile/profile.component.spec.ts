cc5bdb78929b2fac8abec957bf94f3a039b0d6ea
DusanB <dusanbzr@gmail.com>
2020-02-07 20:04:10 +0100

    finish test for profileForm

--- eventsFront/src/app/core/profile/profile.component.spec.ts
+++ eventsFront/src/app/core/profile/profile.component.spec.ts
@@ -18,27 +18,25 @@ fdescribe('ProfileComponent', () => {
   let fixture: ComponentFixture<ProfileComponent>;
 
   let router: Router;
-  let userService: jasmine.SpyObj<UserService>;
-  let authService: jasmine.SpyObj<AuthService>;
+  let userService: any;
+  let authService: any;
   let toastr: ToastrService;
-  let httpClient: jasmine.SpyObj<HttpClient>;
+  let httpClient: any;
 
   beforeEach(async(() => {
-
     const spyAuthService = jasmine.createSpyObj('AuthService', ['getToken']);
     const spyRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
     const httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'put']);
     const spyUserService = new UserService(httpClientSpy, spyAuthService);
-
     TestBed.configureTestingModule({
       declarations: [ ProfileComponent ],
       providers : [
         ToastrService,
         FormBuilder,
-        { provide: UserService, value: spyUserService},
-        { provide: AuthService, value: spyAuthService},
-        { provide: Router, value: spyRouter},
-        { provide: HttpClient, value: httpClientSpy}
+        { provide: UserService, useValue: spyUserService},
+        { provide: AuthService, useValue: spyAuthService},
+        { provide: Router, useValue: spyRouter},
+        { provide: HttpClient, useValue: httpClientSpy}
       ],
       imports: [
         MaterialModule,
@@ -46,12 +44,12 @@ fdescribe('ProfileComponent', () => {
         FormsModule,
         ReactiveFormsModule,
         ToastrModule.forRoot(),
-        RouterModule
+        RouterModule,
+        BrowserAnimationsModule
       ]
-    }).compileComponents();
+    });
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
-    // component = TestBed.get(ProfileComponent);
     userService = TestBed.get(UserService);
     authService = TestBed.get(AuthService);
     toastr = TestBed.get(ToastrService);
@@ -63,7 +61,6 @@ fdescribe('ProfileComponent', () => {
 
     const tokenVal = 'token_sam_ali_najlepsi';
     const myData: User = new User(1, 'Dusan', 'Bucan', 'dusanbzr@gmail.com', '123-12', 'DusanB');
-    console.log(myData);
 
     httpClient.get.and.returnValue(of(myData));
     authService.getToken.and.returnValue(tokenVal);
@@ -78,7 +75,6 @@ fdescribe('ProfileComponent', () => {
 
     // proveri da li mokovane metode dobro rade---> da li si ih dobro mokovao
     expect(authService.getToken.calls.mostRecent().returnValue).toBe(tokenVal);
-    // expect(httpClient.get.calls.mostRecent().returnValue).toBe(of(myData));
 
     // da li su polja forme popunjena validno..
     expect(component.getUserId()).toBe(myData.id);
@@ -87,8 +83,48 @@ fdescribe('ProfileComponent', () => {
     expect(component.email.value).toBe(myData.email);
     expect(component.phone.value).toBe(myData.phone);
     expect(component.username.value).toBe(myData.username);
-
   });
+
+  it('should return updated user token from backEnd', () => {
+
+    const tokenVal = 'token_sam_ali_najlepsi';
+    const myData: User = new User(1, 'Dusan', 'Bucan', 'dusanbzr@gmail.com', '123-12', 'DusanB');
+    const myNewData: User = new User(1, 'Dusann', 'Bucann', 'ddusanbzr@gmail.com', '123-123', 'DusanBB');
+
+    httpClient.get.and.returnValue(of(myData));
+    httpClient.put.and.returnValue(of(tokenVal));
+    authService.getToken.and.returnValue(tokenVal);
+
+    // ovo bi trebalo da pozove metodu iz userService-a me kojem sam prosledio 2
+    // spy objekta i cije metode su iznad defnisane kako treba da se ponasaju...
+    component.ngOnInit();
+
+    // izemenjene vrednosti kao da je korisnik unosio
+    component.name.setValue(myNewData.name);
+    component.surname.setValue(myNewData.surname);
+    component.email.setValue(myNewData.email);
+    component.phone.setValue(myNewData.phone);
+    component.username.setValue(myNewData.username);
+
+    component.onSubmit();
+    expect(localStorage.getItem('token')).toBe(tokenVal);
+  });
+
+  it('should navigate to changePassword component', () => {
+
+    const tokenVal = 'token_sam_ali_najlepsi';
+    const myData: User = new User(1, 'Dusan', 'Bucan', 'dusanbzr@gmail.com', '123-12', 'DusanB');
+    const myNewData: User = new User(1, 'Dusann', 'Bucann', 'ddusanbzr@gmail.com', '123-123', 'DusanBB');
+
+    httpClient.get.and.returnValue(of(myData));
+    httpClient.put.and.returnValue(of(myNewData));
+    authService.getToken.and.returnValue(tokenVal);
+
+    component.ngOnInit();
+    component.changePassword();
+    expect(router.navigateByUrl).toHaveBeenCalledTimes(1);
+  });
+
 
 
 });
