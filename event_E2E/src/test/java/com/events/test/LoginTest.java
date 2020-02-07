327bf6bf710bc29edcb5a19d1df2849a6475f851
DusanB <dusanbzr@gmail.com>
2020-02-07 23:19:59 +0100

    E2E login test finished

--- event_E2E/src/test/java/com/events/test/LoginTest.java
+++ event_E2E/src/test/java/com/events/test/LoginTest.java
@@ -1,47 +1,70 @@
 package com.events.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import com.events.pages.LoginPage;
+import org.junit.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import com.events.config.BrowserFactory;
 import com.events.pages.HomePage;
 import com.events.pages.SingInPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LoginTest {
 
-	private WebDriver browser;
+	private	WebDriver browser;
 
 	HomePage homePage;
-	SingInPage singInPage;
+	LoginPage loginPage;
 
 	@Before
 	public void setupSelenium() throws Exception{
-		
 		browser = BrowserFactory.getBrowser();
-
-		// navigate
-		browser.navigate().to("http://automationpractice.com/index.php");
-
+		loginPage = PageFactory.initElements(browser, LoginPage.class);
 		homePage = PageFactory.initElements(browser, HomePage.class);
-		singInPage = PageFactory.initElements(browser, SingInPage.class);
 	}
 
 	@Test
-	public void singInTest() {
-		homePage.ensureSingInIsDisplayed();
-		homePage.getSingInLink().click();
+	@Order(1)
+	public void A_invalidLoginTest() {
+		browser.navigate().to(LoginPage.FRONT_URL);
+		loginPage.ensureLoginDisplayed();
+		loginPage.setUsernameField("dusann");
+		loginPage.setPasswordField("caoo");
+		loginPage.getLogginBtn().click();
 
-		assertEquals("http://automationpractice.com/index.php?controller=authentication&back=my-account",
+		assertNotEquals(HomePage.FRONT_URL,
 				browser.getCurrentUrl());
 	}
 
-	@After
-	public void closeSelenium() {
+	@Test
+	@Order(2)
+	public void B_validLoginTest() {
+		browser.navigate().to(LoginPage.FRONT_URL);
+		loginPage.ensureLoginDisplayed();
+		loginPage.setUsernameField("dusan");
+		loginPage.setPasswordField("cao");
+		loginPage.getLogginBtn().click();
+		loginPage.ensureLoginNotDisplayed();
+
+		assertEquals(HomePage.FRONT_URL,
+				browser.getCurrentUrl());
+	}
+
+
+
+	@AfterClass
+	public static void closeSelenium() {
 		BrowserFactory.quitBrowser();
 	}
 }
