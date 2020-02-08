cbc5a91a07519eda41075ab79bedd9d6391a31e6
DusanB <dusanbzr@gmail.com>
2020-02-08 18:53:33 +0100

    fixed E2E for ChangePassword

--- event_E2E/src/test/java/com/events/constants/UserConstants.java
+++ event_E2E/src/test/java/com/events/constants/UserConstants.java
@@ -2,7 +2,7 @@ package com.events.constants;
 
 public class UserConstants {
 
-    public static String PASSWORD = "12345678";
+    public static String PASSWORD = "123456789";
     public static String INVALID_PASSWORD = "1234567899";
     public static String VALID_USERNAME = "dusan";
     public static String INVALID_USERNAME = "dusann";
