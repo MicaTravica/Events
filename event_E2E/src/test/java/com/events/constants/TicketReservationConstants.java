221b035cc6975b911bb3e270c15c3e52c53a5384
DusanB <dusanbzr@gmail.com>
2020-02-08 17:01:00 +0100

    finish e2e for TicketReservation

---
+++ event_E2E/src/test/java/com/events/constants/TicketReservationConstants.java
@@ -0,0 +1,13 @@
+package com.events.constants;
+
+public class TicketReservationConstants {
+
+    public static String hallName = "sala1";
+    public static String parterSectorName = "sektor2";
+    public static String seatsSectorName = "sektor3";
+    public static String validNumberOfTicketsParter = "1";
+    public static int validNumberOfSeatTickets = 1;
+    public static String invalidNumberOfTicketsParter = "2000";
+    public static String validMessageReservation = "Reservation successfully made";
+    public static String reservedSeat = "1 | 1";
+}
